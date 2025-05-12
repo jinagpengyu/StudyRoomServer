c8922c18f242a519c217f4fb8ddcc9545972918f
姜棚宇 <1770693880@qq.com>
2025-05-12 19:32:48 +0800

    fix(router): 更新订单状态处理逻辑
    - 修改查询条件，增加对"使用中"和"未使用"状态的订单进行查询
    - 对于日期在今天之前的订单，将状态修改为"已使用"- 对于日期是今天的订单，如果状态为"未使用"，则修改为"使用中"

--- router/interceptor/SeatInterceptor.js
+++ router/interceptor/SeatInterceptor.js
@@ -4,16 +4,33 @@ const UpdateSeatStatus = async (req,res,next) => {
     const orderCollection = getNewCollection('orders')
     const todayYYMMDD = NowYYMMDDString()
     const result = await orderCollection.find({
-        status:"正常"
+        $or:[
+            {
+                status: '使用中'
+            },
+            {
+                status: '未使用'
+            }
+        ]
     }).toArray()
     if(result.length > 0){
         for(let i = 0;i < result.length;i++){
-            if(CompareDate(result[i].order_date,todayYYMMDD) === -1){
+            if(CompareDate(result[i].order_date,todayYYMMDD) === -1 && result[i].status !== '已使用'){
+                // 日期在今天之前的，状态为未使用或者使用中的，则修改为已使用
                 await orderCollection.updateOne({
                     _id:result[i]._id
                 },{
                     $set:{
-                        status:"过期"
+                        status:"已使用"
+                    }
+                })
+            } else if ( CompareDate(result[i].order_date,todayYYMMDD) === 0 && result[i].status === '未使用') {
+                // 日期是今天，状态为未使用，则修改为使用中
+                await orderCollection.updateOne({
+                    _id:result[i]._id
+                },{
+                    $set:{
+                        status:"使用中"
                     }
                 })
             }
